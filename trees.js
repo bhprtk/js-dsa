6e6ee58d7d0cdbd2260d25c88e89ce831a49e741
pratik <lazy.monk.13@gmail.com>
2018-05-30 06:48:25 -0700

    Added search method for BST

--- trees.js
+++ trees.js
@@ -9,6 +9,22 @@ function BinarySearchTree() {
   this.root = null
 }
 
+BinarySearchTree.prototype.search = function(val) {
+  if(!this.root) return null
+  let current = this.root
+  console.log('current', current)
+  while(current) {
+    if(current.val === val){
+      return current
+    } else if(current.val < val) {
+      current = current.right
+    } else if(current.val > val) {
+      current = current.left
+    }
+  }
+  return null
+}
+
 BinarySearchTree.prototype.insert = function(val) {
   if(!Number.isInteger(val)) {
     console.log('You can only insert an integer value.')
@@ -41,8 +57,12 @@ BinarySearchTree.prototype.insert = function(val) {
 }
 
 let tree = new BinarySearchTree()
-console.log('tree', tree)
 tree.insert(5)
 tree.insert(3)
 tree.insert(10)
-console.log('tree', tree)
+tree.insert(1)
+tree.insert(4)
+tree.insert(7)
+tree.insert(12)
+
+console.log('tree.search(3)', tree.search(7))
